bd0f88737c4e35ae28d8b761073eb8b178e254dd
Danielly <daniellygj@gmail.com>
2019-08-12 21:48:53 -0300

    att base projeto

---
+++ src/main/java/hello/Main.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.bson.Document;
+
+import com.mongodb.client.FindIterable;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Model model = new Model();
+		
+		model.addUser(Document.parse("{'name':'pedro', 'password':'12345'}"));
+		model.addUser(Document.parse("{'name':'pedro', 'password':'11111'}"));
+		
+		FindIterable<Document> found = model.searchByName("pedro");
+		
+		for(Document doc:found){
+			System.out.println(doc);
+		}
+	}
+	
+}
