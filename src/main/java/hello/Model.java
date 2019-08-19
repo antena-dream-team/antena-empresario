63618df1ba51d336a6b07a86f4d3a78afba5f4e8
Danielly <daniellygj@gmail.com>
2019-08-19 20:50:08 -0300

    schemes bd closes: #3

--- src/main/java/hello/Model.java
+++ src/main/java/hello/Model.java
@@ -1,33 +1,60 @@
 package hello;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
+
 import com.github.fakemongo.Fongo;
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
 
 
 public class Model {
 	
 	
 	Fongo fongo = new Fongo("mongo");
+	MongoDatabase db = fongo.getDatabase("app");
 	
-	public void addResponsavelEmpresario(Document user) {
-		MongoDatabase db = fongo.getDatabase("app");
-    	MongoCollection<Document> users = db.getCollection("responsavel-empresario");
-    	users.insertOne(user);
+	public void addProjeto(Document projeto) {
+    	
+		MongoCollection<Document> projetos = db.getCollection("projeto");
+    	projetos.insertOne(projeto);
+    	
 	}
 	
-	public void addResponsavelCad(Document user) {
-		MongoDatabase db = fongo.getDatabase("app");
-    	MongoCollection<Document> users = db.getCollection("projeto");
-    	users.insertOne(user);
+	public void updateProjeto(Document projeto) {
+    	
+		MongoCollection<Document> projetos = db.getCollection("projeto");
+    	
+    	BasicDBObject query = new BasicDBObject();
+    	
+    	query.append("_id", projeto.get("_id"));
+		
+    	Bson newDocument = new Document("$set", projeto);
+    	
+    	projetos.findOneAndUpdate(query, newDocument, (new FindOneAndUpdateOptions()).upsert(true));
 	}
 
-	public FindIterable<Document> searchByName(String name) {
-		MongoDatabase db = fongo.getDatabase("app");
-		MongoCollection<Document> users = db.getCollection("responsavel-empresario");
-    	FindIterable<Document> found = users.find(new Document("name", name));
+	
+	public FindIterable<Document> getAllProjetos() {
+    	
+		MongoCollection<Document> projetos = db.getCollection("projeto");
+    	
+		FindIterable<Document> todos = projetos.find();
+		
+		for(Document projeto: todos) {
+			System.out.println(projeto);
+		}
+		
+		return todos;
+	}
+
+	
+	public Document searchByName(String name) {
+		MongoCollection<Document> users = db.getCollection("projeto");
+    	Document found = users.find(new Document("name", name)).first();
     	return found;
     }
 }
