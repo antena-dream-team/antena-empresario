14b32b84f779a1b2f3539cf4100f1ca311694b9a
Danielly <daniellygj@gmail.com>
2019-08-26 22:01:50 -0300

    botao cadastro fonunciando

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    var login = document.getElementById('login');
+    var cadastro = document.getElementById('cadastro');
+    
+    window.onclick = function(event) {
+        if (event.target == login) {
+            login.style.display = "none";
+        }
+
+        if (event.target == cadastro) {
+            cadastro.style.display = "none";
+        }
+    }
+
+    $('#cpf-cadastro').mask('000.000.000-00', {reverse: true});
+
+});
+
+$("#btn-cadastro").click(function(event) {
+
+    event.preventDefault();
+
+//    nome = $("#nome-cadastro").val();
+//    email = $("#email-cadastro").val();
+//    cpf = $("#cpf-cadastro").val();
+//    senha = $("#senha-cadastro").val();
+
+    json = {
+           nome: $("#nome-cadastro").val(),
+           email: $("#email-cadastro").val(),
+           cpf: $("#cpf-cadastro").val(),
+           senha: $("#senha-cadastro").val()
+       }
+
+    jsonString = JSON.stringify(json);
+
+    $.post("/cadastroempresario",
+         jsonString
+    );
+})
+
+function abrePopupLogin(event) {
+    event.preventDefault();
+    document.getElementById('login').style.display='block';    
+}
+
+function fechaPopupLogin(event) {
+    event.preventDefault();
+    document.getElementById('login').style.display='none';    
+}
+
+function abrePopupCadastro(event) {
+    event.preventDefault();
+    document.getElementById('cadastro').style.display='block';    
+}
+
+function fechaPopupCadastro(event) {
+    event.preventDefault();
+    document.getElementById('cadastro').style.display='none';    
+}
